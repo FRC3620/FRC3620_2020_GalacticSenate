10f5dd7c7726c81775c81ffc1b80c4dc63aec460
Horacio Moreno <yayin1111@gmail.com>
2020-02-01 10:41:14 -0500

    Added vision Subsystem as well as a drive and align command that needs to be tested

---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,44 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class VisionSubsystem extends SubsystemBase {
+
+  private NetworkTableInstance inst = NetworkTableInstance.getDefault();
+  private NetworkTable networkTable = inst.getTable("ChickenVision");
+
+  private NetworkTableEntry shootingTargetPresent = networkTable.getEntry("Shooting Centered");
+  private NetworkTableEntry shootingTargetYaw = networkTable.getEntry("Shooting Target Yaw");
+
+  public VisionSubsystem() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public double getShootingTargetYaw(){
+    if (shootingTargetPresent.getBoolean(false)){
+      return shootingTargetYaw.getDouble(0);
+    }
+    else{
+      return 0;
+    }
+  }
+
+  public boolean getShootingTargetPresent(){
+    return shootingTargetPresent.getBoolean(false);
+  }
+}
