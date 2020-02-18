c0424aa03c5b2127c20943a190f0b1b2edcf06e8
537834 <537834@stjoebears.com>
2020-02-17 19:54:04 -0500

    Added MoveLiftCommand to SmartDashboard/ShuffleBoard

--- src/main/java/frc/robot/subsystems/LiftSubsystem.java
+++ src/main/java/frc/robot/subsystems/LiftSubsystem.java
@@ -7,14 +7,15 @@ import org.usfirst.frc3620.logger.EventLogging.Level;
 import org.usfirst.frc3620.misc.LightEffect;
 
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import frc.robot.RobotContainer;
 import frc.robot.commands.MoveLiftCommand;
 /**
- * @author Sean Thursby
- * @version 18 January 2020
+ * @author Sean Thursby & Micah Wagoner
+ * @version 16 February 2020
  */
 public class LiftSubsystem extends SubsystemBase {
   Logger logger = EventLogging.getLogger(getClass(), Level.INFO);
@@ -45,6 +46,8 @@ public class LiftSubsystem extends SubsystemBase {
   
   
   public void liftPower(double speed) { // Runs lift controller based on joystick pos.
+      SmartDashboard.putNumber("Lift Controller Speed", speed);
+
     if (liftController != null) {
       liftController.set(speed); //speed = speed passes through by moveliftcommand
     }
