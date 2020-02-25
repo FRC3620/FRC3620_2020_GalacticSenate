35aa02c71e2e7bda436dd908234ece2f8998de19
Liam Allen <15163@stjoebears.com>
2020-02-24 21:09:32 -0500

    Added comfirmation method to SwerveSettings

--- src/main/java/frc/robot/commands/SaveSwerveAnalogEncodersSettingsCommand.java
+++ src/main/java/frc/robot/commands/SaveSwerveAnalogEncodersSettingsCommand.java
@@ -7,11 +7,17 @@
 
 package frc.robot.commands;
 
+import org.slf4j.Logger;
+import org.usfirst.frc3620.logger.EventLogging;
+import org.usfirst.frc3620.logger.EventLogging.Level;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class SaveSwerveAnalogEncodersSettingsCommand extends CommandBase {
   DriveSubsystem m_subsystem;
+  Logger logger = EventLogging.getLogger(getClass(), Level.INFO);
   /**
    * Creates a new SaveSwerveAnalogEncodersSettingsCommand.
    */
@@ -24,7 +30,14 @@ public class SaveSwerveAnalogEncodersSettingsCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_subsystem.saveCurrentAbsoluteEncoderOffsets();
+    if (RobotContainer.isItOkToRecalibrateTheSwerveHeadings()){
+      m_subsystem.saveCurrentAbsoluteEncoderOffsets();
+      logger.info ("Swerve settings saved!");
+    } else {
+      logger.warn ("Cannot reset the swerve headings unless you hold down the bumpers");
+      RobotContainer.rumbleSubsystemDriver.
+    }
+    
   }
 
   // Called every time the scheduler runs while the command is scheduled.
