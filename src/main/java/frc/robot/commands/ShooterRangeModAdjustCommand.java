c3ba8edfddf4b7ab865aef61f09b1cd3bc53563e
Doug Wegscheid <dwegscheid@sbcglobal.net>
2020-03-04 12:50:12 -0500

    Fix issues #10 and #11 in ShooterRangeModAdjustCommand.java.

--- src/main/java/frc/robot/commands/ShooterRangeModAdjustCommand.java
+++ src/main/java/frc/robot/commands/ShooterRangeModAdjustCommand.java
@@ -7,14 +7,9 @@
 
 package frc.robot.commands;
 
-import com.ctre.phoenix.Logger;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
-import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
-import frc.robot.subsystems.ArmSubsystem.TargetColor;
 
 public class ShooterRangeModAdjustCommand extends CommandBase {
   ShooterSubsystem shooterSubsystem = RobotContainer.shooterSubsystem;
@@ -24,7 +19,6 @@ public class ShooterRangeModAdjustCommand extends CommandBase {
    */
   public ShooterRangeModAdjustCommand(int direction) {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(RobotContainer.shooterSubsystem);
   }
 
   // Called when the command is initially scheduled.
@@ -36,20 +30,16 @@ public class ShooterRangeModAdjustCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-
   }
 
   // Returns true when the command should end.
-  // (To return true the rotation count must be greater than or equal to 4)
   @Override
   public boolean isFinished() {
-
-    return false;
+    return true;
   }
 }
