27c615cc3edac66bf92e5b956a924cc04fa3df75
Doug Wegscheid <dwegscheid@sbcglobal.net>
2021-02-25 21:04:37 -0500

    changed slalam 26

--- src/main/java/frc/robot/commands/Slalom26Command.java
+++ src/main/java/frc/robot/commands/Slalom26Command.java
@@ -27,34 +27,34 @@ public class Slalom26Command extends SlalomCommandGroup{
       new AutoDriveCommand (3.4*12, 90, SPEED, 180, driveSubsystem, "alpha", this)   //alpha
       ,
       new AutoSteerCommand(180, driveSubsystem, "-bravo-", this),
-      new AutoDriveCommand (4*12, 180, SPEED, 180, driveSubsystem, "bravo", this)  // bravo
+      new AutoDriveCommand (4.3*12, 180, SPEED, 180, driveSubsystem, "bravo", this)  // bravo
       ,
       new AutoSteerCommand(90, driveSubsystem, "-charlie-", this),
-      new AutoDriveCommand (11*12, 90, .7, 180, driveSubsystem, "charlie", this)  // Charlie
+      new AutoDriveCommand (11.5*12, 90, .7, 180, driveSubsystem, "charlie", this)  // Charlie
       ,
       new AutoDriveCommand(1*12, 90, SPEED, 180, driveSubsystem),
       //new WaitCommand(1.0),
       new AutoSteerCommand(0, driveSubsystem, "-delta-", this),
-      new AutoDriveCommand (5*12, 0, SPEED, 180, driveSubsystem, "delta", this)   // delta
+      new AutoDriveCommand (4*12, 0, SPEED, 180, driveSubsystem, "delta", this)   // delta
       ,
       new AutoSteerCommand(90, driveSubsystem, "-echo-", this),
-      new AutoDriveCommand (4*12, 90, SPEED, 180, driveSubsystem, "echo", this)  // echo
+      new AutoDriveCommand (4.5*12, 90, SPEED, 180, driveSubsystem, "echo", this)  // echo
       ,
       new AutoSteerCommand(180, driveSubsystem, "-foxtrot-", this),
-      new AutoDriveCommand (5*12, 180, SPEED, 180, driveSubsystem, "foxtrot", this) // foxtrot
+      new AutoDriveCommand (4.0*12, 180, SPEED, 180, driveSubsystem, "foxtrot", this) // foxtrot
       ,
       //new AutoSteerCommand(180, driveSubsystem, "-gamma-", this),
       new AutoSteerCommand(-90, driveSubsystem, "-gamma-", this),
-      new AutoDriveCommand (4.5*12, -90, SPEED, 180, driveSubsystem, "gamma", this)  // gamma
+      new AutoDriveCommand (4*12, -90, SPEED, 180, driveSubsystem, "gamma", this)  // gamma
       ,
       new AutoSteerCommand(0, driveSubsystem, "-hotel-", this),
       new AutoDriveCommand (5*12, 0, SPEED, 180, driveSubsystem, "hotel", this) // hotel
       ,
       new AutoSteerCommand(-90, driveSubsystem, "-india-", this),
-      new AutoDriveCommand (12*12, -90, .5, 180, driveSubsystem, "india", this) // india
+      new AutoDriveCommand (12.25*12, -90, .7, 180, driveSubsystem, "india", this) // india
       ,
       new AutoSteerCommand(180, driveSubsystem, "-juliet-", this),
-      new AutoDriveCommand (4*12, 180, SPEED, 180, driveSubsystem, "juliet", this)  //juliet
+      new AutoDriveCommand (5*12, 180, SPEED, 180, driveSubsystem, "juliet", this)  //juliet
       ,
       new AutoSteerCommand(-90, driveSubsystem, "-kilo-", this),
       new AutoDriveCommand (3*12, -90, SPEED, 180, driveSubsystem, "kilo", this)  // kilo
