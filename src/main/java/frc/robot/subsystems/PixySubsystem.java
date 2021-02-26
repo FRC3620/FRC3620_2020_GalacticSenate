796fec5b8db78bbef4a1411cefe24c9d8946d9c9
Doug Wegscheid <dwegscheid@sbcglobal.net>
2021-02-26 16:04:54 -0500

    Use the Block that comes with pixy library.

--- src/main/java/frc/robot/subsystems/PixySubsystem.java
+++ src/main/java/frc/robot/subsystems/PixySubsystem.java
@@ -8,8 +8,8 @@
 package frc.robot.subsystems;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import io.github.pseudoresonance.pixy2api.Pixy2;
@@ -21,15 +21,14 @@ public class PixySubsystem extends SubsystemBase {
 
   private Pixy2 pixy;
 
-  private BlockDescription lastBlock;
+  private ArrayList<Block> lastBlocks;
 
-  /**
-   * Creates a new PixySubsystem.
-   */
+  static private ArrayList<Block> noBlocks = new ArrayList<>();
+
+  static public Comparator<Block> byDecreasingSize = (b1, b2) -> - ((b1.getWidth() * b1.getHeight()) - (b2.getWidth() * b2.getHeight()));
+
   public PixySubsystem() {
     super();
-
-    lastBlock = new BlockDescription();
 
     pixy = Pixy2.createInstance(new SPILink()); // Creates a new Pixy2 camera using SPILink
     pixy.init(); // Initializes the camera and prepares to send/receive data
@@ -37,62 +36,36 @@ public class PixySubsystem extends SubsystemBase {
     pixy.setLED(255, 255, 255); // Sets the RGB LED to full white
   }
 
-  public static class BlockDescription implements Cloneable {
-    double x, y, height, width;
-    int blockCount; // if 0, we don't see anything!
-    int age, signature;
-
-    public Object clone() throws CloneNotSupportedException {
-      return super.clone();
-    }
-  }
-
   @Override
   public void periodic() {
     // Gets the number of "blocks", identified targets, that match signature 1 on
     // the Pixy2,
     // does not wait for new data if none is available,
-    // and limits the number of returned blocks to 25, for a slight increase in
-    // efficiency
-    lastBlock.blockCount = pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 5);
-    Block largestBlock = null;
-    if (lastBlock.blockCount > 0) {
+    // and limits the number of returned blocks for a slight increase in efficiency
+    int blockCount = pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 25);
+    if (blockCount > 0) {
       ArrayList<Block> blocks = pixy.getCCC().getBlockCache(); // Gets a list of all blocks found by the Pixy2
-      for (Block block : blocks) { // Loops through all blocks and finds the widest one
-        if (largestBlock == null) {
-          largestBlock = block;
-        } else if (block.getWidth() > largestBlock.getWidth()) {
-          largestBlock = block;
-        }
-      }
-      lastBlock.x = largestBlock.getX();
-      lastBlock.y = largestBlock.getY();
-      lastBlock.width = largestBlock.getWidth();
-      lastBlock.height = largestBlock.getHeight();
-      lastBlock.age = largestBlock.getAge();
-      lastBlock.signature = largestBlock.getSignature();
-    }
-    SmartDashboard.putNumber("pixy.count", lastBlock.blockCount);
-    SmartDashboard.putNumber("pixy.x", lastBlock.x);
-    SmartDashboard.putNumber("pixy.y", lastBlock.y);
-    SmartDashboard.putNumber("pixy.width", lastBlock.width);
-    SmartDashboard.putNumber("pixy.signature", lastBlock.signature);
-    SmartDashboard.putNumber("pixy.age", lastBlock.age);
-  }
+      blocks.sort(byDecreasingSize);
+      Block largestBlock = blocks.get(0);
+      SmartDashboard.putNumber("pixy.x", largestBlock.getX());
+      SmartDashboard.putNumber("pixy.y", largestBlock.getY());
+      SmartDashboard.putNumber("pixy.width", largestBlock.getWidth());
+      SmartDashboard.putNumber("pixy.signature", largestBlock.getSignature());
+      SmartDashboard.putNumber("pixy.age", largestBlock.getAge());
 
-  public BlockDescription getBlockDescription() {
-    try {
-      return (BlockDescription) lastBlock.clone();
-    } catch (CloneNotSupportedException ex) {
-      throw new RuntimeException ("could not clone", ex);
+      lastBlocks = blocks;
+    } else {
+      lastBlocks = noBlocks;
     }
+    SmartDashboard.putNumber("pixy.count", blockCount);
   }
 
   public boolean isThereAPowerCell() {
-    if (lastBlock.blockCount == 0) return false;
-    if (lastBlock.signature != 1) return false;
+    if (lastBlocks.size() == 0) return false;
+    Block lastBlock = lastBlocks.get(0);
+    if (lastBlock.getSignature() != 1) return false;
     // small Y are top of frame
-    if (lastBlock.y < 120) return false;
+    if (lastBlock.getY() < 120) return false;
     return true;
   }
 }
