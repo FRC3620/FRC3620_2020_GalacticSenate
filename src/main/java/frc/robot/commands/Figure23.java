215e23f8726665fababa9414f61e57fe8848ab01
Doug Wegscheid <dwegscheid@sbcglobal.net>
2021-02-27 15:57:03 -0500

    added glatic search 24

--- src/main/java/frc/robot/commands/Figure23.java
+++ src/main/java/frc/robot/commands/Figure23.java
@@ -1,10 +1,16 @@
 package frc.robot.commands;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
 
 
 
 public class Figure23 extends SequentialCommandGroup{
+public class Figure23 extends intakeSubsystem{
 
 
 
@@ -12,23 +18,49 @@ public class Figure23 extends SequentialCommandGroup{
 public Figure23(DriveSubsystem driveSubsystem){ 
 
 super();
-
+if(
+//blue
 addCommands(
+    new ZeroDriveEncodersCommand(driveSubsystem),
+    new DeployIntakeCommand(intakeSubsystem),
+    new WaitCommand(.2),
+    new  AutoIntakeArmDownCommand(intakeSubsystem),
+    new AutoStartIntakeCommand(intakeSubsystem),
+new AutoDriveCommand(18*12, 90, .25, 180, driveSubsystem)
+,
+new AutoDriveCommand(5*12, 0, .25, 180, driveSubsystem)
+, 
+new AutoDriveCommand(2.5*12, 90, .25, 180, driveSubsystem)
+,
+new AutoDriveCommand(5*12, 180, .25, 180, driveSubsystem)
+,
+new AutoDriveCommand(2.5*12, 0, .25, 180, driveSubsystem),
+new AutoDriveCommand(5.5*12, 0, .25, 180, driveSubsystem),
+new AutoIntakeArmUPCommand(intakeSubsystem),
+new AutoStopIntakeCommand(intakeSubsystem)
 
-//new AutoDriveCommand(distance, strafeAngle, speed, heading, driveSubsystem)
-
+//red ball
+else 
+new ZeroDriveEncodersCommand(driveSubsystem),
+    new DeployIntakeCommand(intakeSubsystem),
+    new WaitCommand(.2),
+    new  AutoIntakeArmDownCommand(intakeSubsystem),
+    new AutoStartIntakeCommand(intakeSubsystem),
 new AutoDriveCommand(5*12, 90, .25, 180, driveSubsystem)
 ,
-//Pick up ball
-new AutoDriveCommand(5.6*12, 50, .25, 180, driveSubsystem)
+new AutoDriveCommand(2.5*12, 0, .25, 180, driveSubsystem)
+, 
+new AutoDriveCommand(5*12, 90, .25, 180, driveSubsystem)
 ,
-//Pick up ball
-new AutoDriveCommand(8*12, 157.5, .25, 180, driveSubsystem)
+new AutoDriveCommand(7.5*12, 180, .25, 180, driveSubsystem)
 ,
-//Pick up ball
-new AutoDriveCommand(14, 90, .25, 180, driveSubsystem)
+new AutoDriveCommand(20*12, 90, .25, 180, driveSubsystem),
+new AutoIntakeArmUPCommand(intakeSubsystem),
+new AutoStopIntakeCommand(intakeSubsystem)
 
-
+)
 );
 }
 }
+}
+
