5a482f7f455e034d4aa03f95e5b2799968217f26
Doug Wegscheid <dwegscheid@sbcglobal.net>
2021-03-11 20:53:13 -0500

    fixed the figure a blue

---
+++ src/main/java/frc/robot/commands/Figure23PathABlueCommand.java
@@ -0,0 +1,15 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+
+public class Figure23PathABlueCommand extends SlalomCommandGroup {
+
+    public Figure23PathABlueCommand(DriveSubsystem driveSubsystem, IntakeSubsystem intakeSubsystem) {
+        super(driveSubsystem);
+        addCommands(
+               
+              );
+    }
+}
--- src/main/java/frc/robot/commands/NeutroniumAutoCommand.java
+++ src/main/java/frc/robot/commands/NeutroniumAutoCommand.java
@@ -33,10 +33,40 @@ public class NeutroniumAutoCommand extends SequentialCommandGroup {
     this.shooterSubsystem = m_shooterSubsystem;
     this.visionSubsystem = m_visionSubsystem;
     addCommands(
-      new ZeroDriveEncodersCommand(driveSubsystem),
-      new SetHoodPositionCommand(45.0),
-      new MessageCommand (logger,"pretending to run around"),
-      new WaitCommand (2.0),
+
+new ResetNavXCommand(driveSubsystem)
+,
+//new ZeroDriveEncodersCommand(driveSubsystem)
+
+
+new DeployIntakeCommand(intakeSubsystem)
+,
+new WaitCommand(.2)
+,
+      new AutoDriveCommand(4*12,90,75,180, driveSubsystem)
+      ,
+      new AutoDriveCommand(3*12,35,75,180, driveSubsystem)
+      ,
+      new AutoDriveCommand(3*12,90,75,180, driveSubsystem)
+      ,
+      new AutoDriveCommand(2*12,160,75,180, driveSubsystem)
+      ,
+      new AutoDriveCommand(5*12,230,75,180, driveSubsystem)
+      ,
+      new AutoDriveCommand(2*12,150,75,180, driveSubsystem)
+      ,
+      new AutoDriveCommand(14*12, 85,75,180, driveSubsystem)
+
+
+
+
+
+
+
+     // new ZeroDriveEncodersCommand(driveSubsystem),
+     // new SetHoodPositionCommand(45.0),
+     // new MessageCommand (logger,"pretending to run around"),
+     // new WaitCommand (2.0),
 
       /*
       new DeployIntakeCommand(intakeSubsystem),
@@ -44,20 +74,20 @@ public class NeutroniumAutoCommand extends SequentialCommandGroup {
       new AutoDriveCommand(7.3*12, -90, 0.5, 0, driveSubsystem),
       new WaitCommand(0.3),
       */
-      new AutoIntakeArmUpCommand(intakeSubsystem),
+     // new AutoIntakeArmUpCommand(intakeSubsystem),
       /*
       new AutoDriveCommand(2*12, 45, 0.8, 0, driveSubsystem),
       new WaitCommand(.3),
       new AutoDriveCommand(8*12, 7, 0.65, 0, driveSubsystem),
       */
-      new AutoTurnVisionLightOnCommand(visionSubsystem),
+      //new AutoTurnVisionLightOnCommand(visionSubsystem),
       /*
       new AutoStopIntakeCommand(intakeSubsystem),
       new AutoSpinCommand(0.6, -150, driveSubsystem),
       new AutoStartIntakeCommand(intakeSubsystem),
       new WaitCommand(0.4),
       */
-      new MessageCommand (logger, "Aligning..."),
+     /* new MessageCommand (logger, "Aligning..."),
       new DriveAndAlignCommand(driveSubsystem, visionSubsystem),
       new MessageCommand (logger, "Aligned..."),
       new AutoStopIntakeCommand(intakeSubsystem),
