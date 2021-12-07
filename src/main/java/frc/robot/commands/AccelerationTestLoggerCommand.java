bbe86cde77e7825d5964e00f6491f5892c6b581d
Sean Thursby <sthursbyg@gmail.com>
2021-12-06 20:39:12 -0500

    set up AccelerationTestLoggerCommand

--- src/main/java/frc/robot/commands/AccelerationTestLoggerCommand.java
+++ src/main/java/frc/robot/commands/AccelerationTestLoggerCommand.java
@@ -7,9 +7,21 @@
 
 package frc.robot.commands;
 
+import java.util.Date;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import org.usfirst.frc3620.logger.FastDataLoggerCollections;
+import org.usfirst.frc3620.logger.IFastDataLogger;
+
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class AccelerationTestLoggerCommand extends CommandBase {
+  IFastDataLogger accelerationTestLogger;
+  CommandScheduler commandScheduler;
+  TalonFX talonFX;
   /**
    * Creates a new AccelerationTestLoggerCommand.
    */
@@ -20,8 +32,21 @@ public class AccelerationTestLoggerCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    accelerationTestLogger = new FastDataLoggerCollections();
+    accelerationTestLogger.setInterval(0.001);
+    accelerationTestLogger.setMaxLength(10.0);
+    if (talonFX != null) {
+      accelerationTestLogger.addDataProvider("wheelVelocity", () -> talonFX.getSupplyCurrent());
+      accelerationTestLogger.addDataProvider("encoderVelocity", () -> talonFX.getSupplyCurrent());
+    } else {
+      accelerationTestLogger.addDataProvider("t", () -> Timer.getFPGATimestamp());
+    }
+    accelerationTestLogger.setFilename("accelerationTest");
+    accelerationTestLogger.setFilenameTimestamp(new Date());    
+    accelerationTestLogger.start();
   }
-
+  
+  
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
@@ -30,11 +55,15 @@ public class AccelerationTestLoggerCommand extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+  accelerationTestLogger.done();
+  }
+
+  private void done() {
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return commandScheduler.timeSinceScheduled(this) > 2.0;
   }
 }
